eabaad4f7197c3a77e1aeff7a19f46e11e1860de
unknown <danilka2712@yandex.ru>
2023-05-14 22:33:01 +0600

    first commit

--- src/lib/components/Cart.svelte
+++ src/lib/components/Cart.svelte
@@ -1,11 +1,11 @@
 <div class="flex flex-col items-center">
     <div class="flex flex-col items-center mb-2">
         <img src="/cigarette_PNG4759.png" alt="" />
-        <p class=" font-medium">Bond синий compact</p>
+        <p class=" font-medium text-center">Bond синий compact</p>
     </div>
     <div>
         <button
-            class="  bg-[#3478f7] p-2 px-4 font-semibold uppercase text-sm rounded-lg"
+            class="  bg-[#3478f7] text-white p-2 px-4 font-semibold uppercase text-sm rounded-lg"
             >Купить</button
         >
     </div>

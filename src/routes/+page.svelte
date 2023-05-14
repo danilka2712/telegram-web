e8155a9c3a3a7af496c15104cf45793d9f4632ec
unknown <danilka2712@yandex.ru>
2023-05-14 20:55:22 +0600

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    import { browser } from "$app/environment";
+    import { onMount } from "svelte";
+    import { toggle_class } from "svelte/internal";
+
+    interface TelegramWebApp {
+        close: string;
+        init(): void;
+    }
+
+    onMount(() => {
+        if (browser) {
+            const telegram: TelegramWebApp = window.Telegram.WebApp;
+            tg = telegram;
+        }
+    });
+    let tg: any;
+    function close() {
+        tg.close();
+    }
+</script>
+
+<div>
+    <button on:click={close}>Закрыть</button>
+</div>

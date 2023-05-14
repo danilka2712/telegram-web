34eb2a356f65c21bfebd127bb7528997cd68d9b3
unknown <danilka2712@yandex.ru>
2023-05-14 22:21:25 +0600

    first commit

--- src/lib/components/Cart.svelte
+++ src/lib/components/Cart.svelte
@@ -1,18 +1,15 @@
 <div class="flex flex-col items-center">
     <div class="flex flex-col items-center mb-2">
         <img src="/cigarette_PNG4759.png" alt="" />
-        <p class=" font-medium">Bond синий</p>
+        <p class=" font-medium">Bond синий compact</p>
     </div>
     <div>
         <button
-            class=" btn bg-sky-500 p-2 px-4 font-semibold uppercase text-sm rounded-lg"
+            class="  bg-sky-[#3478f7] p-2 px-4 font-semibold uppercase text-sm rounded-lg"
             >Купить</button
         >
     </div>
 </div>
 
 <style>
-    .btn {
-        background-color: var(--tg-theme-button-color);
-    }
 </style>

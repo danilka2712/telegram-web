7d2964b09989c3c8524942f7b004fa74b8a88ac9
unknown <danilka2712@yandex.ru>
2023-05-14 22:47:59 +0600

    first commit

--- src/lib/components/Cart.svelte
+++ src/lib/components/Cart.svelte
@@ -1,3 +1,7 @@
+<script>
+    import Cart from "$lib/svg/Cart.svelte";
+</script>
+
 <div class="flex flex-col items-center">
     <div class="flex flex-col items-center mb-2">
         <img src="/cigarette_PNG4759.png" alt="" />
@@ -5,8 +9,8 @@
     </div>
     <div class="">
         <button
-            class="  bg-[#3478f7] text-white p-2 px-4 font-semibold uppercase text-sm rounded-lg"
-            >Купить</button
+            class="  bg-[#3478f7] text-white flex items-center justify-center gap-2 p-2 px-5 font-semibold uppercase text-sm rounded-lg"
+            ><Cart />920₽</button
         >
     </div>
 </div>

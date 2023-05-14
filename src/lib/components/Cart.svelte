f2d1fdd0fadd5ee791dc1034c910a054ad67dd7e
unknown <danilka2712@yandex.ru>
2023-05-14 22:23:45 +0600

    first commit

--- src/lib/components/Cart.svelte
+++ src/lib/components/Cart.svelte
@@ -5,7 +5,7 @@
     </div>
     <div>
         <button
-            class="  bg-sky-[#3478f7] p-2 px-4 font-semibold uppercase text-sm rounded-lg"
+            class="  bg-[#3478f7] p-2 px-4 font-semibold uppercase text-sm rounded-lg"
             >Купить</button
         >
     </div>

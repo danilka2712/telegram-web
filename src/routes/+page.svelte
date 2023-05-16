7ac2b251f7a447101f68760698a8d6fd86b3d2b2
unknown <danilka2712@yandex.ru>
2023-05-16 23:03:04 +0600

    first commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -24,7 +24,7 @@
 
     function sendTelegramData() {
         const tgApp = window.Telegram.WebApp;
-        tgApp.close();
+        tgApp.sendData(JSON.stringify(dataToSend));
     }
     function toggleMainButton(product) {
         const existingItem = $cart.find((item) => item.id === product.id);

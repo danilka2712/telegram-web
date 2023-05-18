e04e097077ca9cde6e381b6ceacefb6cda689034
unknown <danilka2712@yandex.ru>
2023-05-18 14:41:58 +0600

    first commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -18,6 +18,11 @@
             age: 30,
             email: "john@example.com",
         },
+        {
+            name: "Dane",
+            age: 30,
+            email: "john@example.com",
+        },
     ];
 
     function sendTelegramData() {

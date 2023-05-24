10992e936336fd322bd569334992d99e77d72078
unknown <danilka2712@yandex.ru>
2023-05-24 13:06:46 +0600

    first commit

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -8,10 +8,11 @@
     });
     function sendTelegramData() {
         const tgApp = window.Telegram.WebApp;
-        tgApp.sendData($cart);
+        tgApp.MainButton.sendData($cart);
     }
 </script>
 
+<button on:click={sendTelegramData}>asd</button>
 <div class="body h-screen w-full">
     <div class="box">
         {#each $cart as cart}

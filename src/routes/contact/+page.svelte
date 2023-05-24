8ad931e831c759fc06f196eaebe6afeccc4c618d
unknown <danilka2712@yandex.ru>
2023-05-24 14:58:10 +0600

    first commit

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -21,9 +21,9 @@
         {#each $cart as item}
             <div class="cart-item">
                 <div class="flex items-center">
-                    <div class=" relative bg-white w-20 h-20">
+                    <div class="  bg-white w-20 h-20">
                         <img
-                            class="item-image absolute object-fill w-32 h-32"
+                            class="item-image object-none w-32 h-32"
                             src="cigarette_PNG4759.png"
                             alt=""
                         />

ca7ea1b7af330cf2ee296aad6348cf4c1b1f7718
unknown <danilka2712@yandex.ru>
2023-05-24 13:12:30 +0600

    first commit

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -6,17 +6,12 @@
         const tgApp = window.Telegram.WebApp;
         tgApp.onEvent("mainButtonClicked", sendTelegramData);
     });
-    let hide = false;
     function sendTelegramData() {
         const tgApp = window.Telegram.WebApp;
         tgApp.sendData($cart);
-        hide = true;
     }
 </script>
 
-{#if hide}
-    <h1>dsadsad</h1>
-{/if}
 <div class="body h-screen w-full">
     <div class="box">
         {#each $cart as cart}
